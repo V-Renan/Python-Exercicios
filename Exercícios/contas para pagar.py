a5a58fed67f910efa619c4ce8342c905fc824f1a
V-Renan <victorrenan2018@gmail.com>
2023-07-29 18:11:43 -0300

    att

--- Exercícios/contas para pagar.py
+++
@@ -1,9 +0,0 @@
-total = 0
-r = ' '
-while True:
-    valor = int(input("Valor: "))
-    total += valor
-    r = str(input("Deseja continuar? ")).strip().upper()[0]
-    if r == "N":
-        break
-print(f"O total é igual a: R${total:.2f}")
