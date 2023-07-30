61df4a23a731291235b89dca4a085479eacc49dd
V-Renan <victorrenan2018@gmail.com>
2023-07-29 22:19:50 -0300

    up

--- Exercícios/desafio aula 07D.py
+++ Exercícios/desafio aula 07D.py
@@ -1,7 +1,4 @@
 n1 = float(input('Quer calcular sua média?\nDigite a primeira nota:'))
 n2 = float(input('Digite a segunda nota: '))
 m = (n1+n2)/2
-#print (m)
-print ('A sua media é {:.1f}'.format(m))
-
-
+print('A sua media é {:.1f}'.format(m))
